d1bb313c25e6b101e62afef7aa1d7a3ab7cde765
student44965076bc <student44965076bc@harness.io>
2024-06-01 00:14:43 +0000

    Add sto day 2 c3 terraform files

--- security-test-orchestration/harness-terraform-sto/day-two/c3/main.tf
+++ security-test-orchestration/harness-terraform-sto/day-two/c3/main.tf
@@ -11,10 +11,10 @@ variable "account_id" {}
 variable "org_id" {}
 variable "project_id" {}
 variable "pat" {}
-variable "refpipeline" {}
+variable "refpipeline1" {}
+variable "refpipeline2" {}
+variable "container_registry_link" {}
 variable "refinputset" {}
-variable "refinputsetgittrigger" {}
-variable "refgittrigger" {}
 
 provider "harness" {
     endpoint            = "https://app.harness.io/gateway"
@@ -22,16 +22,43 @@ provider "harness" {
     platform_api_key    = var.pat
 }
 
-resource "harness_platform_pipeline" "autopipeline" {
+resource "harness_platform_pipeline" "autopipeline1" {
   org_id        = var.org_id
   project_id    = var.project_id
-  name          = var.refpipeline
-  identifier    = var.refpipeline
-  yaml = templatefile("pipeline.yaml", {
+  name          = var.refpipeline1
+  identifier    = var.refpipeline1
+  git_details {
+    branch_name    = "main"
+    commit_message = "Create ${var.refpipeline1} in my-harness-configs-${var.project_id}"
+    file_path      = ".harness/${var.refpipeline1}.yaml"
+    store_type     = "REMOTE"
+    repo_name      = "my-harness-configs-${var.project_id}"
+  }
+  yaml = templatefile("pipeline1.yaml", {
     org_identifier = var.org_id
     project_identifier = var.project_id
-    pipeline_name = var.refpipeline
-    pipeline_identifier = var.refpipeline
+    pipeline_identifier = var.refpipeline1
+    container_registry_link = var.container_registry_link
+  })
+}
+
+resource "harness_platform_pipeline" "autopipeline2" {
+  org_id        = var.org_id
+  project_id    = var.project_id
+  name          = var.refpipeline2
+  identifier    = var.refpipeline2
+  git_details {
+    branch_name    = "main"
+    commit_message = "Create ${var.refpipeline2} in my-harness-configs-${var.project_id}"
+    file_path      = ".harness/${var.refpipeline2}.yaml"
+    store_type     = "REMOTE"
+    repo_name      = "my-harness-configs-${var.project_id}"
+  }  
+  yaml = templatefile("pipeline2.yaml", {
+    org_identifier = var.org_id
+    project_identifier = var.project_id
+    pipeline_identifier = var.refpipeline2
+    container_registry_link = var.container_registry_link
   })
 }
 
@@ -40,52 +67,28 @@ resource "harness_platform_input_set" "inputset" {
   project_id    = var.project_id
   name          = var.refinputset
   identifier    = var.refinputset
-  pipeline_id   = harness_platform_pipeline.autopipeline.id
+  pipeline_id   = harness_platform_pipeline.autopipeline2.id
+  git_details {
+    branch_name    = "main"
+    commit_message = "Create ${var.refinputset} in my-harness-configs-${var.project_id}"
+    file_path      = ".harness/${var.refinputset}.yaml"
+    store_type     = "REMOTE"
+    repo_name      = "my-harness-configs-${var.project_id}"
+  }
   yaml          = templatefile("inputset.yaml", {
     org_identifier = var.org_id
     project_identifier = var.project_id
     inputset_name = var.refinputset
     inputset_identifier = var.refinputset
-    pipeline_identifier = harness_platform_pipeline.autopipeline.id
-  })
-}
-
-resource "harness_platform_input_set" "inputsetgittrigger" {
-  org_id        = var.org_id
-  project_id    = var.project_id
-  name          = var.refinputsetgittrigger
-  identifier    = var.refinputsetgittrigger
-  pipeline_id   = harness_platform_pipeline.autopipeline.id
-  yaml          = templatefile("inputsetforgittrigger.yaml", {
-    org_identifier = var.org_id
-    project_identifier = var.project_id
-    inputset_name = var.refinputsetgittrigger
-    inputset_identifier = var.refinputsetgittrigger
-    pipeline_identifier = harness_platform_pipeline.autopipeline.id
-  })
-}
-
-resource "harness_platform_triggers" "gittrigger" {
-  org_id        = var.org_id
-  project_id    = var.project_id
-  name          = var.refgittrigger
-  identifier    = var.refgittrigger
-  target_id     = harness_platform_pipeline.autopipeline.id
-  yaml          = templatefile("gittrigger.yaml", {
-    org_identifier = var.org_id
-    project_identifier = var.project_id
-    trigger_name = var.refgittrigger
-    trigger_identifier = var.refgittrigger
-    inputset_identifier = harness_platform_input_set.inputsetgittrigger.id
-    pipeline_identifier = harness_platform_pipeline.autopipeline.id
+    pipeline_identifier = harness_platform_pipeline.autopipeline2.id
+    container_registry_link = var.container_registry_link
   })
 }
 
 output "myoutput" {
   value       = {
-    pipeline1 = harness_platform_pipeline.autopipeline.id
-    inputset1 = harness_platform_input_set.inputset.id
-    inputset2 = harness_platform_input_set.inputsetgittrigger.id
-    trigger1  = harness_platform_triggers.gittrigger.id
+    pipeline1 = harness_platform_pipeline.autopipeline1.id
+    pipeline2 = harness_platform_pipeline.autopipeline2.id
+    inputset = harness_platform_input_set.inputset.id
   }
 }
