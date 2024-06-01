df23cb60ac5c274a38ef4905e808b445a0f313ad
student44965076bc <student44965076bc@harness.io>
2024-06-01 02:55:17 +0000

    Add TF scripts for STO day-2 c4

--- security-test-orchestration/harness-terraform-sto/day-two/c4/main.tf
+++ security-test-orchestration/harness-terraform-sto/day-two/c4/main.tf
@@ -12,6 +12,8 @@ variable "org_id" {}
 variable "project_id" {}
 variable "pat" {}
 variable "refpipeline" {}
+variable "refinputset" {}
+variable "refstepgrouptemplate" {}
 
 provider "harness" {
     endpoint            = "https://app.harness.io/gateway"
@@ -29,11 +31,45 @@ resource "harness_platform_pipeline" "autopipeline" {
     project_identifier = var.project_id
     pipeline_name = var.refpipeline
     pipeline_identifier = var.refpipeline
+    template_identifier = var.refstepgrouptemplate
+  })
+  depends_on = [harness_platform_template.stepgrouptemplate]
+}
+
+resource "harness_platform_input_set" "inputset" {
+  org_id        = var.org_id
+  project_id    = var.project_id
+  name          = var.refinputset
+  identifier    = var.refinputset
+  pipeline_id   = harness_platform_pipeline.autopipeline.id
+  yaml          = templatefile("inputset.yaml", {
+    org_identifier = var.org_id
+    project_identifier = var.project_id
+    inputset_name = var.refinputset
+    inputset_identifier = var.refinputset
+    pipeline_identifier = harness_platform_pipeline.autopipeline.id
+  })
+}
+
+resource "harness_platform_template" "stepgrouptemplate" {
+  org_id        = var.org_id
+  project_id    = var.project_id
+  name          = var.refstepgrouptemplate
+  identifier    = var.refstepgrouptemplate
+  comments      = ""
+  version       = "1.0.0"
+  is_stable     = true
+  template_yaml = templatefile("stepgrouptemplate.yaml", {
+    org_identifier = var.org_id
+    project_identifier = var.project_id
+    template_identifier = var.refstepgrouptemplate
   })
 }
 
 output "myoutput" {
   value       = {
     pipeline1 = harness_platform_pipeline.autopipeline.id
+    inputset = harness_platform_input_set.inputset.id
+    stepgrouptemplate = harness_platform_template.stepgrouptemplate.id
   }
 }
