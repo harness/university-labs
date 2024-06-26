e30130fb2e7393ecdbd47bf9adb6c22b6c10efcd
hv-harness <149600354+hv-harness@users.noreply.github.com>
2024-06-26 18:09:40 -0400

    Harsh Transition

--- repos/my-terraform/random-pet/main.tf
+++
@@ -1,5 +0,0 @@
-resource "random_pet" "my-pet" {
-  prefix = var.prefix
-  separator = var.separator
-  length = var.length
-}
--- repos/my-terraform/random-pet/output.tf
+++
@@ -1,5 +0,0 @@
-output pet-name {
-  value = random_pet.my-pet.id
-  sensitive = false
-  description = "Print the name of the pet"
-}
