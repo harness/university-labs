2e09fce9ae9743e440d93bf02557ef534c438df0
Wyatt Munson <wyatt.munson@harness.io>
2024-10-21 17:32:11 -0700

    add: help command to CE lab

--- chaos-engineering/scripts/docs.sh
+++ chaos-engineering/scripts/docs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -e "\n===== HARNESS ILT TRAINING =====\n"
-echo -e "Commands to run\n"
-echo -e "debug\t\tList course-based variables"
-echo -e "mytools\t\tPrint list of installed binaries/tools on this instance"
-echo -e "settoken\tRead a Harness PAT token and set to variable"
-echo -e "gettoken\tGet the value and check Harness PAT token is set"
-echo -e "testconn\tInitiate a 'test connection' command to the k8s connector"
-echo -e "docs\t\tThis command"
+echo -e "Available commands"
+echo -e "  debug\t\tList common course environment variables"
+echo -e "  mytools\tList of installed binaries/tools on this instance"
+echo -e "  settoken\tRead a Harness PAT token and set to variable"
+echo -e "  gettoken\tGet the value and check Harness PAT token is set"
+echo -e "  testconn\tInitiate a 'test connection' command to the k8s connector"
+echo -e "  docs\t\tThis help command"
